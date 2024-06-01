54dbe69550e1e8584427da8113ff9c5c9e1c11fd
dduc3 <dduc31767@gmail.com>
2024-06-01 15:23:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pnlib/model/PhieuMuon.java
@@ -0,0 +1,102 @@
+package com.example.pnlib.model;
+
+import java.util.Date;
+
+public class PhieuMuon {
+    private int maPM;
+    private String maTT;
+    private int maTV;
+    private int maSach;
+    private Date ngay;
+    private int tienThue;
+    private int traSach;
+    private int thueVat;
+
+    public PhieuMuon() {
+    }
+
+    public PhieuMuon(int maPM, String maTT, int maTV, int maSach, Date ngay, int tienThue, int traSach, int thueVat) {
+        this.maPM = maPM;
+        this.maTT = maTT;
+        this.maTV = maTV;
+        this.maSach = maSach;
+        this.ngay = ngay;
+        this.tienThue = tienThue;
+        this.traSach = traSach;
+        this.thueVat = thueVat;
+    }
+
+    public int getThueVat() {
+        return thueVat;
+    }
+
+    public void setThueVat(int thueVat) {
+        this.thueVat = thueVat;
+    }
+
+    public PhieuMuon(int maPM, String maTT, int maTV, int maSach, Date ngay, int tienThue, int traSach) {
+        this.maPM = maPM;
+        this.maTT = maTT;
+        this.maTV = maTV;
+        this.maSach = maSach;
+        this.ngay = ngay;
+        this.tienThue = tienThue;
+        this.traSach = traSach;
+    }
+
+    public int getMaPM() {
+        return maPM;
+    }
+
+    public void setMaPM(int maPM) {
+        this.maPM = maPM;
+    }
+
+    public String getMaTT() {
+        return maTT;
+    }
+
+    public void setMaTT(String maTT) {
+        this.maTT = maTT;
+    }
+
+    public int getMaTV() {
+        return maTV;
+    }
+
+    public void setMaTV(int maTV) {
+        this.maTV = maTV;
+    }
+
+    public int getMaSach() {
+        return maSach;
+    }
+
+    public void setMaSach(int maSach) {
+        this.maSach = maSach;
+    }
+
+    public Date getNgay() {
+        return ngay;
+    }
+
+    public void setNgay(Date ngay) {
+        this.ngay = ngay;
+    }
+
+    public int getTienThue() {
+        return tienThue;
+    }
+
+    public void setTienThue(int tienThue) {
+        this.tienThue = tienThue;
+    }
+
+    public int getTraSach() {
+        return traSach;
+    }
+
+    public void setTraSach(int traSach) {
+        this.traSach = traSach;
+    }
+}
